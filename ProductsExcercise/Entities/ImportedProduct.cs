994b75766d4a52aa8dc56f624436b9b93f64d8ac
Henrique Mendes <henriquemendes13@gmail.com>
2024-01-13 02:51:24 -0300

    Add project files.

---
+++ ProductsExcercise/Entities/ImportedProduct.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace ProductsExcercise.Entities
+{
+    internal class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct() 
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customsFee)
+        {
+            base.Name = name;
+            this.Price = price;
+            CustomsFee = customsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return Name + " $" + TotalPrice().ToString() + $" (Customs Fee: ${CustomsFee.ToString()})";
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+    }
+}
---
+++ ProductsExcercise/Entities/UsedProduct.cs
@@ -0,0 +1,25 @@
+﻿
+namespace ProductsExcercise.Entities
+{
+    internal class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price, DateTime manufactureDate)
+        {
+            base.Name = name;
+            base.Price = price;
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return Name + " (Used) $" + Price.ToString() + $" (Manufacture Date: {ManufactureDate.ToString()})";
+        }
+    }
+        
+}
